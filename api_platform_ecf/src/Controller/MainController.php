5a4baefef737a315440163cac6db569a95c43ad7
Thery <j.thery2311@outlook.fr>
2019-02-14 14:50:32 +0100

    add items-index.html.twig

--- api_platform_ecf/src/Controller/MainController.php
+++ api_platform_ecf/src/Controller/MainController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+    private $conn;
+
+    public function __construct(Connection $conn)
+    {
+        $this->conn = $conn;
+    }
     /**
      * @Route("/main", name="main")
      */
